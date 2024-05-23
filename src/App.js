f203f1a8435711ab4adf4b817f76e492e3509d06
Jammi-UwU-Jammy <jamclark102@gmail.com>
2024-05-23 02:18:28 -0400

    added different sections

--- src/App.js
+++ src/App.js
@@ -1,6 +1,42 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+/* ============ SHOW MENU ============ */
+const   navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close')
+
+if (navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* ============ HIDE MENU ============ */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/* ============ REMOVE ON MENU CLICK ============ */
+const navLink = document.querySelectorAll('.nav__link')
+const linkAction = () =>{
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/* ============ REMOVE ON MENU CLICK ============ */
+const shadowHeader = () =>{
+  const header = document.getElementById('header')
+  console.log(header)
+  window.scrollY >= 50 ? header.classList.add('shadow-header')
+                     : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
+
+
 function App() {
   return (
     <p>
